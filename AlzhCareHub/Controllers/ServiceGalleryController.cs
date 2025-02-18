de7c33a630fd127d983ea6daa17ff9fd33f57eec
ShazaibAbuBasharMernStackdeveloper2210 <142742492+shazaibabubashar2210@users.noreply.github.com>
2025-02-19 00:03:28 +0500

    I add some more pages like Contact Page Remiander page and change more structure

---
+++ AlzhCareHub/Controllers/ServiceGalleryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlzhCareHub.Controllers
+{
+    public class ServiceGalleryController : Controller
+    {
+        public IActionResult Remainder_And_Alerts()
+        {
+            return View();
+        }
+    }
+}
