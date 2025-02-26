646eec8f84710c5791923c66c3e75c2b6cd030a7
ShazaibAbuBasharMernStackdeveloper2210 <142742492+shazaibabubashar2210@users.noreply.github.com>
2025-02-26 16:37:31 +0500

    I have added the feedback feature with the admin respond backend

---
+++ AlzhCareHub/Models/FeedBack.cs
@@ -0,0 +1,10 @@
+﻿namespace AlzhCareHub.Models
+{
+    public class FeedbackModel
+    {
+        public string? Id { get; set; }
+        public string? UserEmail { get; set; }
+        public string? Message { get; set; }
+        public string? Response { get; set; } // Admin Response
+    }
+}
---
+++ AlzhCareHub/Models/FireBaseHelper.cs
@@ -0,0 +1,86 @@
+﻿using Firebase.Auth;
+using Firebase.Database;
+using Firebase.Database.Query;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AlzhCareHub.Models
+{
+    public class FirebaseHelper
+    {
+        private static string ApiKey = "AIzaSyCWeoUTihsySNUteX_FWBu9mDMXJY57yUs";
+        private static string DatabaseUrl = "https://alzheimercarehubsystem-a42e6-default-rtdb.firebaseio.com/";
+        private static FirebaseAuthProvider authProvider = new FirebaseAuthProvider(new FirebaseConfig(ApiKey));
+        private static FirebaseClient firebaseClient = new FirebaseClient(DatabaseUrl);
+
+        // Submit Feedback
+        public static async Task<bool> SubmitFeedback(FeedbackModel feedback)
+        {
+            try
+            {
+                feedback.Id = Guid.NewGuid().ToString(); // Assign unique ID
+                await firebaseClient
+                    .Child("Feedbacks")
+                    .Child(feedback.Id)
+                    .PutAsync(feedback);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        // Get All Feedbacks
+        public static async Task<List<FeedbackModel>> GetAllFeedbacks()
+        {
+            try
+            {
+                var feedbacks = await firebaseClient
+                    .Child("Feedbacks")
+                    .OnceAsync<FeedbackModel>();
+
+                List<FeedbackModel> feedbackList = new List<FeedbackModel>();
+                foreach (var feedback in feedbacks)
+                {
+                    feedbackList.Add(feedback.Object);
+                }
+
+                return feedbackList;
+            }
+            catch (Exception)
+            {
+                return new List<FeedbackModel>();
+            }
+        }
+
+        // Respond to Feedback
+        public static async Task<bool> RespondToFeedback(string id, string responseMessage)
+        {
+            try
+            {
+                var feedback = await firebaseClient
+                    .Child("Feedbacks")
+                    .Child(id)
+                    .OnceSingleAsync<FeedbackModel>();
+
+                if (feedback != null)
+                {
+                    feedback.Response = responseMessage;
+                    await firebaseClient
+                        .Child("Feedbacks")
+                        .Child(id)
+                        .PutAsync(feedback);
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
